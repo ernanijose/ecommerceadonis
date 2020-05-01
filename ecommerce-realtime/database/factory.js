edd973e4aaa5f429c301da3a99e064107bb2c516
ernanijose <ernanijose@gmail.com>
2020-05-01 20:36:55 -0300

    Inciando as Factory com o Faker

--- ecommerce-realtime/database/factory.js
+++ ecommerce-realtime/database/factory.js
@@ -19,3 +19,27 @@
 //     username: faker.username()
 //   }
 // })
+
+Factory.blueprint('App/Models/User', (faker) => {
+    return {
+        name: faker.first(),
+        surname: faker.last(),
+        email: faker.email({ domain: 'olhachei.com.br'}),
+        password: 'secret'
+    }
+})
+
+Factory.blueprint('App/Models/Category', (faker) => {
+    return {
+        title: faker.country({ full: true }),
+        description: faker.sentence()
+    }
+})
+
+Factory.blueprint('App/Models/Product', (faker) => {
+    return {
+        name: faker.animal(),
+        description: faker.sentence(),
+        price: faker.floating({ min: 0, max: 1000, fixed: 2 })
+    }
+})
